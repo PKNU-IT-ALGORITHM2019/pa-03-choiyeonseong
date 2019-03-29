749a4ba390f662df6677ebfdbc97d31ff4d86b4e
choiyeonseong <am3251@naver.com>
2019-03-29 21:39:43 +0900

    hi

---
+++ ConsoleApplication1/ConsoleApplication1/swap.cpp
@@ -0,0 +1,24 @@
+#include "Çì´õ.h"
+void swap(int *a, int *b) {
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void RandomData(int data[], int N) {
+	for (int i = 0; i < N; i++) {
+		data[i] = rand() % (N - 1);
+	}
+}
+
+void ReversedData(int data[], int N) {
+	for (int i = 0; i<N; i++) {
+		data[i] = N - i;
+	}
+}
+
+void arrcpy(int data1[], int data2[],int N) {
+	for (int i = 0; i < N; i++) {
+		data1[i] = data2[i];
+	}
+}
